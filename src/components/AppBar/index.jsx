98db3627fca33670410fe5cf2ddeb2e65b5cbf25
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-10-31 11:48:27 +0700

    Add layout app bar.

---
+++ src/components/AppBar/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+
+const AppBar = () => {
+  return (
+    <Container>
+      <img className="h-[120px]" src={"/logo.svg"} />
+    </Container>
+  );
+};
+
+export default AppBar;
