a686c3d50dbc81d84ae959b4739abde64e6c9de3
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2025-01-08 17:32:54 +0700

    Complete login and logout.

--- src/components/AppBar/menu.jsx
+++ src/components/AppBar/menu.jsx
@@ -4,11 +4,22 @@ import {
   SearchOutlined,
   ShoppingBagOutlined,
 } from "@mui/icons-material";
-import { Stack, Typography } from "@mui/material";
-import useAuth from "../../hooks/useAuth";
+import { MenuItem, Stack, Typography } from "@mui/material";
+import { Menu as MuiMenu } from "@mui/material";
+import { useState } from "react";
+import { useAuth } from "../../context/auth";
 
 const Menu = () => {
-  const { isLogin } = useAuth();
+  const { isLogin, login, logout } = useAuth();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Stack
@@ -25,19 +36,42 @@ const Menu = () => {
           <SearchOutlined sx={{ width: 40 }} />
           <FavoriteBorderOutlined sx={{ width: 40 }} />
           <ShoppingBagOutlined sx={{ width: 40 }} />
-          <PersonOutline sx={{ width: 40 }} />
+          <div onClick={handleClick}>
+            <PersonOutline sx={{ width: 40 }} />
+          </div>
+          <MuiMenu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                logout();
+              }}
+            >
+              Logout
+            </MenuItem>
+          </MuiMenu>
         </>
       ) : (
-        <Typography
-          sx={{
-            fontSize: 18,
-            cursor: "pointer",
-            fontFamily: "Montserrat",
-            color: "#000",
-          }}
-        >
-          SIGN IN
-        </Typography>
+        <div onClick={login}>
+          <Typography
+            sx={{
+              fontSize: 18,
+              cursor: "pointer",
+              fontFamily: "Montserrat",
+              color: "#000",
+            }}
+          >
+            SIGN IN
+          </Typography>
+        </div>
       )}
     </Stack>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import RouterProvider from "./routers";
 import MuiProvider from "./configs/mui/index";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./context/auth";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MuiProvider>
-      <RouterProvider />
-    </MuiProvider>
+    <ToastContainer />
+    <AuthProvider>
+      <MuiProvider>
+        <RouterProvider />
+      </MuiProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
